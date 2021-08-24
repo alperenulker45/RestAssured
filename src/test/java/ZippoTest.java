6756b458b9c43cdac91f22249d8dde3e2004c752
alperenulker45” <ulkeralperen@gmail.com>
2021-08-24 21:29:52 +0300

    ilk ayarlar yapildi

---
+++ src/test/java/ZippoTest.java
@@ -0,0 +1,22 @@
+
+import org.testng.annotations.Test;
+
+import static io.restassured.RestAssured.*;
+import static org.hamcrest.Matchers.*;
+
+public class ZippoTest {
+
+    @Test
+    public void test() {
+
+        given()
+                //Hazirlik islemlerini yapacagiz
+                .when()
+                //link ve aksiyon islemleri
+
+                .then()
+                //test ve extract islemleri
+        ;
+
+    }
+}
