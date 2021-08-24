8443d8268545b34bf7308f2de4e608742da98e4d
alperenulker45” <ulkeralperen@gmail.com>
2021-08-24 22:06:41 +0300

    statusCode added

--- src/test/java/ZippoTest.java
+++ src/test/java/ZippoTest.java
@@ -11,12 +11,34 @@ public class ZippoTest {
 
         given()
                 //Hazirlik islemlerini yapacagiz
+
                 .when()
                 //link ve aksiyon islemleri
 
                 .then()
                 //test ve extract islemleri
         ;
+
+    }
+
+    @Test
+    public void statusCodeTest() {
+
+        given()
+
+
+                .when()
+                .get("http://api.zippopotam.us/us/90210")
+
+                        .then()
+                        .log().body() // log.all() --> tum bilgileri yazar. body sadece body icindeki
+
+
+
+
+        ;
+
+
 
     }
 }
