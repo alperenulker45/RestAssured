2bd69488230dc05067cd06ce8fcd64c4467d8e08
alperenulker45” <ulkeralperen@gmail.com>
2021-09-04 21:54:13 +0300

    task4-5-6-7

--- src/test/java/Task.java
+++ src/test/java/Task.java
@@ -1,6 +1,9 @@
+import POJO.Todo;
 import io.restassured.http.ContentType;
 import org.testng.Assert;
 import org.testng.annotations.Test;
+
+import java.util.Arrays;
 
 import static io.restassured.RestAssured.*;
 import static org.hamcrest.Matchers.*;
@@ -56,6 +59,151 @@ public class Task {
         Assert.assertTrue(body.equalsIgnoreCase("203 Non-Authoritative Information"));
 
     }
+
+    /** Task 3
+     *  create a request to https://jsonplaceholder.typicode.com/todos/2
+     *  expect status 200
+     *  expect content type JSON
+     *  expect title in response body to be "quis ut nam facilis et officia qui"
+     */
+
+    @Test
+    public void task3() {
+
+
+        given()
+
+                .when()
+                .get("https://jsonplaceholder.typicode.com/todos/2")
+
+                .then()
+                .log().body()
+                .statusCode(200)
+                .contentType(ContentType.JSON)
+                .body("title", equalTo("quis ut nam facilis et officia qui"))
+
+                ;
+
+    }
+
+    /** Task 4
+     * create a request to https://jsonplaceholder.typicode.com/todos/2
+     *  expect status 200
+     *  expect content type JSON
+     *  expect response completed status to be false
+     */
+
+    @Test
+    public void task4() {
+
+        given()
+
+                .when()
+                .get("https://jsonplaceholder.typicode.com/todos/2")
+
+                .then()
+                .log().body()
+                .statusCode(200)
+                .contentType(ContentType.JSON)
+                .body("completed", equalTo(false));
+
+
+    }
+
+    /** Task 5
+     * create a request to https://jsonplaceholder.typicode.com/todos
+     * expect status 200
+     * expect content type JSON
+     * expect third item have:
+     *      title = "fugiat veniam minus"
+     *      userId = 1
+     */
+
+    @Test
+    public void task5() {
+
+        given()
+
+                .when()
+                .get("https://jsonplaceholder.typicode.com/todos")
+
+                .then()
+                .statusCode(200)
+                .contentType(ContentType.JSON)
+                .log().body()
+                .body("title[2]", equalTo("fugiat veniam minus"))
+                .body("userId[2]", equalTo(1))
+
+                ;
+
+
+
+    }
+
+    /** Task 6
+     * create a request to https://jsonplaceholder.typicode.com/todos/2
+     * expect status 200
+     * Converting Into POJO
+     */
+
+    @Test
+    public void task6() {
+
+        Todo todo =
+
+        given()
+
+                .when()
+                .get("https://jsonplaceholder.typicode.com/todos/2")
+
+                .then()
+                .log().body()
+                .statusCode(200)
+                .extract().as(Todo.class)
+
+                ;
+
+
+        System.out.println("todo = " + todo);
+        System.out.println("todo.getTitle() = " + todo.getTitle());
+        System.out.println("todo.getId() = " + todo.getId());
+        System.out.println("todo.getClass() = " + todo.getUserId());
+
+
+
+    }
+
+
+    /** Task 7
+     * create a request to https://jsonplaceholder.typicode.com/todos
+     * expect status 200
+     * Converting Array Into Array of POJOs
+     */
+
+    @Test
+    public void task7() {
+
+        Todo[] todos=
+
+
+        given()
+
+                .when()
+                .get("https://jsonplaceholder.typicode.com/todos")
+
+                .then()
+                .log().status()
+                .statusCode(200)
+                .extract().as(Todo[].class)
+
+                ;
+
+        System.out.println("Arrays.toString(todos) = " + Arrays.toString(todos));
+
+
+    }
+
+
 }
 
 
