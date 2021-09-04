1afbce33516cdc3545e3070f66f7a149e80dc0f7
alperenulker45” <ulkeralperen@gmail.com>
2021-09-04 21:01:05 +0300

    task2

--- src/test/java/Task.java
+++ src/test/java/Task.java
@@ -1,4 +1,5 @@
 import io.restassured.http.ContentType;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 import static io.restassured.RestAssured.*;
@@ -27,4 +28,34 @@ public class Task {
                 ;
 
     }
+
+    /* Task 2
+            * create a request to https://httpstat.us/203
+            * expect status 203
+            * expect content type TEXT
+     * expect BODY to be equal to "203 Non-Authoritative Information"
+            */
+
+    @Test
+    public void task2() {
+
+        String body=
+
+        given()
+
+                .when()
+                .get("https://httpstat.us/203")
+
+                .then()
+                .log().body()
+                .contentType(ContentType.TEXT)
+                //.body(equalTo("203 Non-Authoritative Information"))
+                .extract().body().asString()
+                ;
+
+        Assert.assertTrue(body.equalsIgnoreCase("203 Non-Authoritative Information"));
+
+    }
 }
+
+
